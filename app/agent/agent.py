d244d19eb64bd2e016ca4c273a2c70e0d529c416
xhacu-bejaj <xhacubejaj@gmail.com>
2026-02-06 19:16:24 +0100

    refactor: change system prompt

--- app/agent/agent.py
+++ app/agent/agent.py
@@ -2,13 +2,13 @@ from langchain.agents import create_agent
 from langchain_ollama import ChatOllama
 from langchain.agents.structured_output import ToolStrategy
 
-from app.agent.tools import weather_tool
+from app.agent.tools import get_weather_by_location # Only import the super-tool
 from app.models.schemas import DailyWeatherParameters
 
 SYSTEM_PROMPT = """
     You are a helpful weather assistant that provides accurate
-    and concise weather forecasts based on user requests. 
-    Use the provided tools to fetch weather data as needed.
+    and concise weather forecasts based on user requests.
+    Use the get_weather_by_location tool to retrieve the weather forecast for any specified location.
     """
 
 
@@ -19,7 +19,7 @@ model = ChatOllama(
 
 agent = create_agent(
     model,
-    tools=[weather_tool],
+    tools=[get_weather_by_location], 
     response_format=ToolStrategy(DailyWeatherParameters),
     system_prompt=SYSTEM_PROMPT,
 )
